80e8cbe8f1bbf027d87ed89f101ed573bbde4737
adityapatel5001 <rishurocks95@gmail.com>
2026-03-10 12:16:20 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+
+
+resource "aws_instance" "example" {
+  ami           = "ami-06e3c045d79fd65d9"
+  instance_type = "t3.micro"
+  key_name = "my-key-pair"
+
+
+  tags = {
+    Name = "AppNode1"
+  }
+}
