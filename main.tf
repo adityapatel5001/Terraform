93bd7da990968042a9dd5c40859af91823daa3de
adityapatel5001 <rishurocks95@gmail.com>
2026-03-12 01:41:16 -0400

    fix terraform syntax

--- main.tf
+++ main.tf
@@ -1,36 +1,13 @@
-
 provider "aws" {
   region = "us-east-2"
 }
 
-
-
-
 resource "aws_instance" "example" {
   ami           = "ami-06e3c045d79fd65d9"
   instance_type = "t3.micro"
-  key_name = "my-key-pair"
-
+  key_name      = "my-key-pair"
 
   tags = {
     Name = "AppNode1_test"
   }
-
-provider "aws" {
-  region = "us-east-2"
-}
-
-
-
-
-resource "aws_instance" "example" {
-  ami           = "ami-06e3c045d79fd65d9"
-  instance_type = "t3.micro"
-  key_name = "my-key-pair"
-
-
-  tags = {
-    Name = "AppNode1"
-  }
-
 }
