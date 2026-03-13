bbd42bff1d613eb8891937d048ecb01015d40eaa
adityapatel5001 <rishurocks95@gmail.com>
2026-03-13 01:24:57 -0400

    change the tag for autorestart

--- main.tf
+++ main.tf
@@ -7,16 +7,14 @@ provider "aws" {
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  for_each = toset(["one", "two"])
-
-  name = "instance-${each.key}"
+  name = "instance_one"
 
   instance_type = "t3.micro"
   key_name      = "my-key-pair"
   subnet_id     = "subnet-0c900516d8719d873"
 
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    autorestart = "true"
+
   }
 }
